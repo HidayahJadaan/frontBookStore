788264c1d5f10cf7e4abd84856ea0fbea2a90754
HidayahJadaan <hkaljadaan18@cit.just.edu.jo>
2026-04-16 17:44:02 +0300

    Header is Done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,28 +1,14 @@
 import React from 'react'
 import './header.css'
+import HeaderTop from './HeaderTop'
+import HeaderMiddle from './HeaderMiddle'
 function Header() {
     return (
         <header className='header'>
-            <div className="header-top">
-                <div className="header-top-menu">
+            
+<HeaderTop/>
+          <HeaderMiddle />
 
-                    <i className="bi bi-list"></i>
-
-                </div>
-
-                <div className="header-top-phone">
-                    079-767-7693
-                </div>
-
-                <div className="header-top-text">
-                    Welcome to online book store
-                </div>
-
-                <div className="header-top-link">
-                    <i className="bi bi-person-fill"></i>
-                    login
-                </div>
-            </div>
         </header>
     )
 }
---
+++ src/components/header/HeaderTop.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export default function HeaderTop() {
+  return (
+   <div className="header-top">
+                <div className="header-top-menu">
+
+                    <i className="bi bi-list"></i>
+
+                </div>
+
+                <div className="header-top-phone">
+                    <i className="bi bi-telephone-fill"></i>
+                    079-767-7693
+                </div>
+
+                <div className="header-top-text">
+                    Welcome to online book store
+                </div>
+
+                <div className="header-top-link">
+                    <i className="bi bi-person-fill"></i>
+                    login
+                </div>
+            </div>
+  )
+}
